b126a46e7af125d7981efe4d5cf011d0f77c106c
pawankp550 <pawankp550@gmail.com>
2020-04-09 23:22:59 +0530

    ProductById populate category

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -47,7 +47,7 @@ exports.create = async (req, res) => {
 
 exports.getProductById = async (req, res) => {
     try{
-        const product = await Product.findById(req.params.id)
+        const product = await Product.findById(req.params.id).populate("category")
 
         if(!product){
             return res.status(404).json({
