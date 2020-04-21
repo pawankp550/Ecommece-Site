419bfb5aa860acaeecf923b8c664c247b5d677d7
pawankp550 <pawankp550@gmail.com>
2020-04-21 12:18:51 +0000

    update product quantity middleware

--- src/controllers/order.js
+++ src/controllers/order.js
@@ -13,7 +13,7 @@ exports.createOrder = async (req, res) => {
                     });
                 }
 
-                res.status(200).send(savedOrder)
+                res.status(200).json({savedOrder})
         } catch (err) {
             res.status(500).json({
             error: errorHandler(err)
--- src/routes/order.js
+++ src/routes/order.js
@@ -1,8 +1,9 @@
 const router = require('express').Router()
 const auth = require('../middleware/auth')
+const { addOrderToUserHistory, updateProductQuantity } = require('../middleware/order')
 const { createOrder } = require('../controllers/order')
 
 // create order
-router.post('/order/create', auth, createOrder)
+router.post('/order/create', auth, addOrderToUserHistory, updateProductQuantity, createOrder)
 
 module.exports = router
